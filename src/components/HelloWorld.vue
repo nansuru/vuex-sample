8e3180e33824a972428887167f98514ccafe854e
DESKTOP-H3L3I4U\ficti <madebyrh@gmail.com>
2021-01-12 00:31:35 +0900

    list5-28

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,14 @@
   <div class="alert alert-primary">
     <h1>{{ data.title }}</h1>
     <p class="mt-3 h5">{{ $store.state.message }}</p>
+    <hr />
+    <div
+      class="btn btn-secondary"
+      @click="$store.commit('count')"
+      @click.ctrl="$store.commit('reset')"
+    >
+      <a class="h5"> clicked: {{ $store.state.counter }} </a>
+    </div>
   </div>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -4,6 +4,15 @@ export const store = createStore({
   state() {
     return {
       message: "This is store data.",
+      counter: 0,
     };
+  },
+  mutations: {
+    count: (state) => {
+      state.counter++;
+    },
+    reset: (state) => {
+      state.counter = 0;
+    },
   },
 });
